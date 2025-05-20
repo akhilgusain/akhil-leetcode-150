b3e55be10877a2cc59253104f669934a84cd6fc7
akhilgusain <akhilgusain555@gmail.com>
2025-05-20 21:01:46 +0530

    271 encode decode strings

---
+++ strings/271EncodeDecodeString.java
@@ -0,0 +1,31 @@
+package strings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    // we need to encode strings like str = ['abc', 'ab', 'fr'] => 'abc#ab#fr'
+    public String encode(List<String> strs) {
+
+        if(strs.size() == 0) return Character.toString((char)258); // return string out of 257 ASCII value
+
+        String seperate = Character.toString((char)257);
+        StringBuilder sb = new StringBuilder();
+        
+        for(String s : strs){
+            sb.append(s);
+            sb.append(seperate);
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+    // decodes a single string to a list of strings
+    public List<String> decode(String s){
+
+        if(s.equals(Character.toString((char)258))) return new ArrayList<>();
+
+        String seperate = Character.toString((char)257);
+        return Arrays.asList(s.split(seperate, -1));
+    }
+}
