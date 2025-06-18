d90c9064894f515f6e5ab724c389f52aca499a9f
akhilgusain <akhilgusain555@gmail.com>
2025-06-18 22:31:08 +0530

    22 Generate Parenthesis

--- dynamic_programming/22GenerateParenthesis.java
+++ dynamic_programming/22GenerateParenthesis.java
@@ -3,6 +3,14 @@ package dynamic_programming;
 import java.util.ArrayList;
 import java.util.List;
 
+// two conditions are there to be followed
+// the sum of open and close should be equal to n
+// open should be less than n
+// close should be less than open
+// i.e ex: for n = 2 then (()) ()() here we can see that in any point if we want to add open paranthesis
+// we have to check it should not be greater than n.
+// also for any close we can not exceed open brackets 
+
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
