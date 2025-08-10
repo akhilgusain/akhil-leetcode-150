824b73250f23b406a20bd573c2cb72e934ce29f2
akhilgusain <akhilgusain555@gmail.com>
2025-08-10 13:34:11 +0530

    104 Maximum Depth of Binary Tree

---
+++ trees/104MaximumDepthOfBinaryTree.java
@@ -0,0 +1,35 @@
+package trees;
+
+
+//       1          (depth 1)
+//      / \
+//     2   3       (depth 2)
+//    / \
+//   4   5         (depth 3)
+
+/**
+ * Definition for a binary tree node. **/
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+class Solution {
+    public int solve(TreeNode root, int depth){
+        if(root == null) return depth;
+        int left = solve(root.left, depth + 1);
+        int right = solve(root.right, depth + 1);
+        return Math.max(left, right);
+    }
+    public int maxDepth(TreeNode root) {
+        return solve(root, 0);
+    }
+}
---
+++ trees/226InvertBinaryTree.java
@@ -0,0 +1,32 @@
+package trees;
+/**
+ * Definition for a binary tree node. */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        // since its a classic recursion scenario, we will repeat for left and right
+        // we are gonna treat each node as root node in subsequent recursion
+        if(root == null) return root;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        
+        return root;
+    }
+}
