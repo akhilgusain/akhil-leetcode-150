82c5d3be7e6c4e032a1b2c5704f3dac6d7493a32
akhilgusain <akhilgusain555@gmail.com>
2025-08-16 18:44:26 +0530

    572 Subtree Of Another Tree

---
+++ trees/TreeNode.java
@@ -0,0 +1,33 @@
+package trees;
+
+/**
+ * Definition for a binary tree node.
+ **/
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+class Solution {
+    public boolean isSame(TreeNode s, TreeNode t){
+        if(s == null && t == null) return true;
+        if(s == null || t == null) return false;
+        if(s.val != t.val) return false;
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null) return false;
+        if(isSame(root, subRoot)) return true;
+        // now for the root we have to check left and right
+        // since subtree can exist in either left or right we put || condition
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+}
