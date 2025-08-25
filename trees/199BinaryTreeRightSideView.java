daf97051d0dfbf48b511641a113a1b7548b5b0db
akhilgusain <akhilgusain555@gmail.com>
2025-08-25 22:57:53 +0530

    199 Binary Tree Right Side View

---
+++ trees/199BinaryTreeRightSideView.java
@@ -0,0 +1,55 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ **/
+ class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                // add last node's value of each level to the result list
+                if (i == levelSize - 1)
+                    result.add(currentNode.val);
+                // add child nodes to the left or right
+                if (currentNode.left != null)
+                    queue.add(currentNode.left);
+                if (currentNode.right != null)
+                    queue.add(currentNode.right);
+            }
+        }
+        return result;
+    }
+}
